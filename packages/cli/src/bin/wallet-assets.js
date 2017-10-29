f338d1f5a5e717629e14d2a32c3bdc9f964c105b
Andy Wermke <andy@dev.next-step-software.com>
2017-10-29 18:59:31 +0100

    Show aliases in `wallet assets` output & make it prettier

--- packages/cli/src/bin/wallet-assets.js
+++ packages/cli/src/bin/wallet-assets.js
@@ -1,6 +1,7 @@
 import chalk from 'chalk'
 import program from 'commander'
 import { loadOrCreateStore } from 'key-store'
+import { padEnd, sortBy } from 'lodash'
 import { loadSDK } from '@wallet/sdk'
 import pkg from '../../package.json'
 import { implementations, keyStorePath } from '../config'
@@ -20,9 +21,21 @@ async function printAvailableAssets () {
   const keyStore = await loadOrCreateStore(keyStorePath)
   const sdk = loadSDK(keyStore, implementations)
 
+  const formatAliases = aliases => {
+    if (aliases.length === 0) {
+      return chalk.grey('No aliases')
+    } else {
+      const formattedAliases = sortBy(aliases).map(alias => chalk.blue(alias)).join(', ')
+      return `${chalk.grey('Aliases:')} ${formattedAliases}`
+    }
+  }
+
   console.log('Supported assets:')
+  console.log('')
 
   for (const asset of sdk.assets) {
-    console.log(`  ${chalk.blue(asset.id)} ${chalk.grey(asset.name)}`)
+    console.log(`  ${chalk.blue(padEnd(asset.id, 4))}\t${chalk.grey(asset.name)}`)
+    console.log(`  ${formatAliases(asset.aliases)}`)
+    console.log('')
   }
 }
