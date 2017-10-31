77607b54af941cbdcb5203285ed48e284ed791ef
Andy Wermke <andy@dev.next-step-software.com>
2017-10-31 15:29:07 +0100

    SDK shall be written in TypeScript

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,10 +1,11 @@
 import { flatMap } from 'lodash'
+import { KeyStore, Implementation, Asset } from '@wallet/implementation-api'
 
 const defaultRequestPassword = () => {
   throw new Error('No requestPassword() function passed to loadSDK().')
 }
 
-export function loadSDK (keyStore, implementations, { requestPassword = defaultRequestPassword } = {}) {
+export function loadSDK (keyStore: KeyStore, implementations: Implementation[], { requestPassword = defaultRequestPassword } = {}) {
   const assets = flatMap(implementations, implementation => implementation.getAssets())
 
   return {
