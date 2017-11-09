d91382fd134f177a4ed7e0ec895f4ce0cd9aa207
Andy Wermke <andy@dev.next-step-software.com>
2017-11-09 21:22:58 +0100

    Fix CLI command name: wallet => zaster

--- packages/cli/test/helpers/shell.ts
+++ packages/cli/test/helpers/shell.ts
@@ -2,7 +2,7 @@ import * as path from 'path'
 import execa = require('execa')
 
 const pkg = require('../../package.json')
-const walletToolPath = path.join(__dirname, '..', '..', pkg.bin.wallet)
+const walletToolPath = path.join(__dirname, '..', '..', pkg.bin.zaster)
 
 export default function shell (command, options = {}) {
   command = command.replace(/^zaster\b/, `node ${walletToolPath}`)
