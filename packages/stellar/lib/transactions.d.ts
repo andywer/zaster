43df9bdea1ae7feabecab75535fe2b4797bfba50
Andy Wermke <a.wermke@gmail.com>
2017-11-15 21:02:52 +0100

    Git-ignore d.ts files (#16)
    * Remove *.d.ts files from git repo
    
    * Git-ignore all files in lib/ directories, not just lib/**/*.js

--- packages/stellar/lib/transactions.d.ts
+++
@@ -1,5 +0,0 @@
-import { Operation, Transaction, Wallet } from '@wallet/platform-api';
-export declare function createTransaction(wallet: Wallet, operations: Operation[], options?: {}): Promise<Transaction>;
-export declare function sendTransaction(transaction: Transaction, options?: {
-    testnet?: boolean;
-}): Promise<Transaction>;
